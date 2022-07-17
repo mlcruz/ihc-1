4d2684ec2d3f5f15f2e235e8ae5dc2a7f28c4284
Matheus Cruz <=>
2022-07-17 14:03:34 -0300

    1-3

--- App.tsx
+++ App.tsx
@@ -1,38 +1,71 @@
-import React, {useState} from 'react';
-import {Button, SafeAreaView, Text, TextInput} from 'react-native';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, Text, TextInput, View} from 'react-native';
+
+import {
+  gyroscope,
+  setUpdateIntervalForType,
+  SensorTypes,
+} from 'react-native-sensors';
 
 export const App = () => {
   return <MainScreen />;
 };
 
+type PageState =
+  | {
+      state: 'get-gyro';
+      coord: {x: number; y: number; z: number};
+    }
+  | {
+      state: 'show-message';
+      message: string;
+    };
+
 const MainScreen = () => {
-  const [state, setState] = useState<number[]>([0, 0]);
-  const [somaState, setSomaState] = useState('');
+  const [state, setState] = useState<PageState>({
+    state: 'get-gyro',
+    coord: {x: 0, y: 0, z: 0},
+  });
+
+  useEffect(() => {
+    setUpdateIntervalForType(SensorTypes.accelerometer, 400); // defaults to 100ms
+    const subscription = gyroscope.subscribe(speed => {
+      if (speed.x >= 1 || speed.y >= 1 || speed.z >= 1) {
+        setState({state: 'show-message', message: 'girando'});
+        setTimeout(() => {
+          setState({state: 'get-gyro', coord: {x: 0, y: 0, z: 0}});
+        }, 10000);
+      } else if (state.state !== 'show-message') {
+        setState({
+          state: 'get-gyro',
+          coord: {
+            ...speed,
+          },
+        });
+      }
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
 
   return (
     <SafeAreaView>
-      <TextInput
-        placeholder="Enter a number"
-        value={state[0].toString()}
-        onChangeText={text => {
-          const digits = text.replace(/\D/g, '');
-
-          setState([+digits, state[1]]);
-        }}
-      />
-      <TextInput
-        placeholder="Enter a number"
-        value={state[1].toString()}
-        onChangeText={text => {
-          const digits = text.replace(/\D/g, '');
-          setState([state[0], +digits]);
-        }}
-      />
-      <Button
-        title="Somar"
-        onPress={() => setSomaState((state[0] + state[1]).toString())}
-      />
-      <Text>{somaState}</Text>
+      {state.state === 'get-gyro' ? (
+        <>
+          <View>
+            <TextInput value={state.coord.x.toString()} />
+            <TextInput value={state.coord.y.toString()} />
+            <TextInput value={state.coord.z.toString()} />
+          </View>
+        </>
+      ) : (
+        <>
+          <Text>{state.message}</Text>
+        </>
+      )}
     </SafeAreaView>
   );
 };
